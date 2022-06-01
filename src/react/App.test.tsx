7be0b384c0de30d6ec1f325275f4271919f21323
viliussapauskas <vilius.sapauskas.1997@gmail.com>
2022-06-01 21:29:35 +0300

    App snapshot

--- src/react/App.test.tsx
+++ src/react/App.test.tsx
@@ -1,17 +1,7 @@
-import React from "react";
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "../redux/store";
-// test("renders learn react link", () => {
-//   const { getByText } = render(
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   );
-
-//   expect(getByText(/learn/i)).toBeInTheDocument();
-// });
 
 const renderComponent = () => {
   return render(
@@ -25,6 +15,4 @@ describe("App tests", () => {
   it("Should render component", () => {
     expect(renderComponent()).toMatchSnapshot();
   });
-
-  // it("Should have two routes");
 });
