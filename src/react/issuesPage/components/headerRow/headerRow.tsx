0b92e453a0acd1cd78e05ebbdacb02f99866dab5
viliussapauskas <vilius.sapauskas.1997@gmail.com>
2022-06-01 22:12:12 +0300

    Header row + additional styling

--- src/react/issuesPage/components/headerRow/headerRow.tsx
+++ src/react/issuesPage/components/headerRow/headerRow.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from "react";
+import styles from "./styles.module.scss";
 
 interface HeaderRowProps {
   openIssues: number;
 }
 
 export const HeaderRow: FC<HeaderRowProps> = ({ openIssues }) => {
-  return <div>HeaderRow {openIssues}</div>;
+  return (
+    <div data-testid="header-row-root" className={styles.container}>
+      <div className={styles.iconSection}>
+        <span className={styles.elipse}>
+          <span className={styles.dot} />
+        </span>
+      </div>
+      <div className={styles.contentSection}>
+        <span className={styles.title}>{openIssues} Open</span>
+      </div>
+    </div>
+  );
 };
-
-// export default HeaderRow
